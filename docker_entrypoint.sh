4c362f9c35a698e55b62ecbaedca9250c62a941d
Tahir Mahmood <tahirmahmood@tahirmacbookpro.home>
2017-08-25 22:51:39 +0100

    echo starting

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd /pipeline/source/app
 echo "starting"
-# mkdir /pipeline/source/app/publish/tmp
-# export TEMP=/pipeline/source/app/publish/tmp
-export TMPDIR=/pipeline/source/app
-dotnet helloworld.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
